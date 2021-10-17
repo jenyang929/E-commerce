2257b2f58f14d90ab2d51f70c89f8cbaca93c1aa
Jennifer Yang <jyang@artemisag.com>
2021-10-17 19:17:29 -0400

    Add tests to app.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,45 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  test("renders a list of brands", () => {
+    render(<App />);
+    const brands = screen.getAllByRole("checkbox");
+    expect(brands).toHaveLength(8);
+  });
+
+  test("toggles unchecked select to checked", () => {
+    render(<App />);
+    const brands = screen.getAllByRole("checkbox")[0];
+    expect(brands.value).toBe("false");
+    fireEvent.click(brands);
+    const brandsAfter = screen.getAllByRole("checkbox")[0];
+    expect(brandsAfter.value).toBe("true");
+  });
+
+  test("toggles multiple unchecked selected to checked", () => {
+    render(<App />);
+    const brand1 = screen.getAllByRole("checkbox")[0];
+    const brand2 = screen.getAllByRole("checkbox")[1];
+    fireEvent.click(brand1);
+    fireEvent.click(brand2);
+    const brand1After = screen.getAllByRole("checkbox")[0];
+    const brand2After = screen.getAllByRole("checkbox")[1];
+    expect(brand1After.value).toBe("true");
+    expect(brand2After.value).toBe("true");
+  });
+
+  test("purchase brands on purchase button", () => {
+    render(<App />);
+    const brand2 = screen.getAllByRole("checkbox")[2];
+    const brand3 = screen.getAllByRole("checkbox")[3];
+    fireEvent.click(brand2);
+    fireEvent.click(brand3);
+    const purchaseButton = screen.getByText("Purchase!");
+    fireEvent.click(purchaseButton);
+    const receipt = screen.getByText("Receipt 1");
+    expect(receipt).toBeVisible();
+    const list = screen.getByText("CB2, JoyBird");
+    expect(list).toBeVisible();
+  });
 });
